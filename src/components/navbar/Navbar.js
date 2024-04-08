4d81f5c56d4fc9d5936173de878a35b16c217847
ibtisam-adil <ibtisamadil273@gmail.com>
2024-04-08 15:42:47 +0500

    Created Navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import './navbar.css';
+
+const Navbar = () => (
+  <div>
+    <nav className="navbar">
+      <div className="math-fans">
+        <p>Math Fans</p>
+      </div>
+      <div className="links">
+        <ul>
+          <li>Home</li>
+          <li>Calculator</li>
+          <li>Quotes</li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+);
+
+export default Navbar;
